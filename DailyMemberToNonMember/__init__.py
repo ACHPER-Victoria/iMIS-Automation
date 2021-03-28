b37b44bf978fe0af3eff314bda9152022e6a76f1
Lee <clam@nyanya.org>
2021-03-29 01:25:06 +1100

    Initial publish

---
+++ DailyMemberToNonMember/__init__.py
@@ -0,0 +1,26 @@
+import datetime
+import logging
+from os import environ
+
+import azure.functions as func
+
+from ..iMIS import api, helpers
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+    IQAQUERY = environ["IQAQUERY"]
+
+    if mytimer.past_due:
+        logging.info('DailyMemberToNonMember is past due!')
+
+    # build list of IDs to nuke:
+    toNMIDs = []
+    for item in api.IterateQuery(IQAQUERY):
+        # Change all these results to NM Non-member
+        toNMIDs.append(item["ID"])
+    for id in toNMIDs:
+        pobj = api.getContact(id)
+        helpers.genericProp(pobj, "CustomerTypeCode", "NM", "AdditionalAttributes")
+        api.updateContact(pobj, id)
+    logging.info('DailyMemberToNonMember completed at %s', utc_timestamp)
---
+++ DailyMemberToNonMember/readme.md
@@ -0,0 +1,8 @@
+# iMIS - Automation
+Various things to make iMIS life easier
+
+## DailyMemberToNonMember
+This task converts specific users returned in an IQA query to non-member status. (Useful for purging certain types of "temporary" or "pending" kind of members.)
+
+## Generate Renewals
+TBC - This would be nice
